982cb14f3a3ba3861b9d73d3800d5dbd0188f1f3
yangfengyi <648331026@qq.com>
2024-06-10 22:53:57 +0800

    用户下单功能代码

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderMapper {
+    /**
+     * 插入订单数据
+     * @param order
+     */
+    void insert(Orders order);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+}
